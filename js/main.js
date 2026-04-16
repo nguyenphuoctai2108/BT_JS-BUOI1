ef8bbe01767740015758bc381cdadd5b943f51eb
nguyenphuoctai2108 <0899974647npt@gmail.com>
2026-04-16 19:39:10 +0700

    upload source

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Giả sử: n = 97
+ * lấy số hàng đơn vị: 7
+ * lấy số hàng chục: 9
+ * => 7 + 9 = 16
+ *
+ * Đầu vào: nhập n = 97
+ * Xử lí:
+ * tạo biến hàng chục = lấy n / 10
+ * làm tròn hàng chục = Math.floor
+ * tạo biến hàng dv = lấy n % 10
+ * total = hàng chục + hàng dv
+ * tạo biến rs để thiết kế câu thông báo
+ * Đầu ra: show kết quả ra ngoài thẻ p
+ */
+let btnTongKySo = document.getElementById("btnTongKySo");
+btnTongKySo.onclick = function () {
+  let number_n = document.getElementById("number_n").value;
+  let hangChuc = Math.floor(number_n / 10);
+  let hangDonvi = number_n % 10;
+  let total = hangChuc + hangDonvi;
+
+  let rs = `Kết quả: <span>${total}</span>`;
+  pInfo = document.getElementById("pInfo");
+  pInfo.innerHTML = rs; // do rs là span dấu đô la
+  pInfo.classList.add("showInfo");
+};
